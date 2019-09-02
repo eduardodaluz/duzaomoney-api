42906336dcfa50303a834387c92fa7273ccf5f70
Eduardo <eduardo@webner.com.br>
2019-09-02 20:15:55 -0300

    Aula 01

---
+++ src/main/groovy/br/com/duzao/money/api/DuzaomoneyApiApplication.groovy
@@ -0,0 +1,13 @@
+package br.com.duzao.money.api
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DuzaomoneyApiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DuzaomoneyApiApplication, args)
+	}
+
+}
---
+++ src/test/groovy/br/com/duzao/money/api/DuzaomoneyApiApplicationTests.groovy
@@ -0,0 +1,16 @@
+package br.com.duzao.money.api
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class DuzaomoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
